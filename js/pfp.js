3a19b843bde517aadafdb21b222686bb8e8f0f19
Boofi <79476279+boofiboi@users.noreply.github.com>
2022-04-30 20:20:11 +0200

    chance to get random image for slejsa and dynamic pfp for me

--- js/pfp.js
+++ js/pfp.js
@@ -15,4 +15,31 @@ let pfp = function(){
     $("img.boofipfp").attr("src", jsonParsed.avatar);
     $("#trollec").text(jsonParsed.usernamewtag);
 
+    var randomnum = Math.floor(Math.random() * 5);
+    let slejsa = function(){   
+        console.log(randomnum)
+        switch(randomnum){
+case 0: 
+$("img.slejsapfp").attr("src", "slejsik.png");
+console.log("set0");
+break;
+case 1:
+$("img.slejsapfp").attr("src", "slejsik1.png");
+console.log("set1");
+break;
+case 2:
+$("img.slejsapfp").attr("src", "slejsik2.png");
+console.log("set2");
+break;
+case 3:
+$("img.slejsapfp").attr("src", "slejsik3.png");
+console.log("set3");
+break;
+case 4:
+$("img.slejsapfp").attr("src", "slejsik4.png");
+console.log("set4");
+break;
+        }   
+    }
+    slejsa();
 }
